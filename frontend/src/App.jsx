7ff0515915e61e081d42d33e3d5eb9488e32eb21
Syed Abubakar Ali <syedabubakarali@gmail.com>
2024-10-28 10:20:44 +0500

    Updated Services and animations

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,7 +3,7 @@ import Header from "./components/common/Header";
 import Footer from "./components/common/Footer";
 import { ThemeProvider } from "@mui/material/styles";
 import { CssBaseline } from "@mui/material";
-import Container from "./components/common/container";
+import Container from "./components/common/Container";
 import theme from "./components/common/theme";
 
 const App = () => {
